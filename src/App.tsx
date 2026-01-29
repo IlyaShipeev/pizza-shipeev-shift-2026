95af9785a3c87fd7a2677ab447dcff5ee694ea43
Ilya.Shipeev <ishipeev@inbox.ru>
2026-01-29 18:23:45 +0700

    feature: add header and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,35 @@
 import { Provider } from 'react-redux'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import { theme } from './styles/theme'
+import theme from './styles/theme'
 import { store } from './store'
 import { ROUTES } from './constants/routes'
-import Header from './components/Header/Header'
+import  Header  from './components/Header/Header'
 import './App.scss'
-
+import { AuthLayout } from './layouts/AuthLayout/AuthLayout'
+import {MainLayout} from './layouts/MainLayout/MainLayout'
 function App() {
 
 
   return (
-    <Provider store = {store}>  кто уже начал делать задания учитывайте, что к пути картинки нужно добавить
+    <Provider store = {store}> 
       <ThemeProvider theme= {theme}>
         <CssBaseline /> 
         <BrowserRouter>
         <div className="app-container">
           <Header />
           <main className='app-content'>
-          <Routes>
-            <Route path={ROUTES.HOME} element= {<div>Pizza App</div>} />
+          <Routes >
+            <Route element={<AuthLayout />}>
+              <Route path={ROUTES.LOGIN} element= {<div>Login</div>} />
+              <Route path={ROUTES.REGISTER} element= {<div>Register</div>} />
+            </Route>
+            <Route element={<MainLayout />}>
+            <Route path={ROUTES.HOME} element= {<></>} />
             <Route path={ROUTES.PROFILE} element= {<div>Profile</div>} /> 
             <Route path={ROUTES.ORDERS} element= {<div>Orders</div>} />
             <Route path={ROUTES.CART} element= {<div>Cart</div>} />
-            <Route path={ROUTES.LOGIN} element= {<div>Login</div>} />
-            <Route path={ROUTES.REGISTER} element= {<div>Register</div>} />
+            </Route>
           </Routes> 
           </main>
         </div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,61 @@
-interface HeaderProps {
-    
-}
+import { AppBar, Toolbar, IconButton, Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import WatchLaterOutlinedIcon from "@mui/icons-material/WatchLaterOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import { ROUTES } from "../../constants/routes";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NavButton from "../ui/NavButton"; // Предполагается, что NavButton в папке ui
+import styles from "./Header.module.scss";
 
-export default function Header({}:HeaderProps) {
+export default function Header() {
+    const navigate = useNavigate();
+    const cartItemsCount = 3;
+
     return (
-        <header>
-        </header>
+        <AppBar position="fixed" className={styles.header}>
+            <Toolbar className={styles.toolbar}>
+                <Box className={styles.logoSection}>
+                    <IconButton
+                        onClick={() => navigate(ROUTES.HOME)}
+                        className={styles.logoButton}
+                        disableRipple
+                    >
+                        <img
+                            src="src\assets\images\image.png"
+                            className={styles.img}
+                            alt="Логотип"
+                        />
+                    </IconButton>
+                </Box>
+
+                <Box className={styles.nav}>
+                    <Box className={styles.navSection}>
+                        <NavButton
+                            icon={<PersonOutlineOutlinedIcon />}
+                            text="Профиль"
+                            onClick={() => navigate(ROUTES.PROFILE)}
+                        />
+                        <NavButton
+                            icon={<WatchLaterOutlinedIcon />}
+                            text="Заказы"
+                            onClick={() => navigate(ROUTES.ORDERS)}
+                        />
+                    </Box>
+
+                    <NavButton
+                        icon={<ShoppingCartOutlinedIcon/>}
+                        text="Корзина"
+                        onClick={() => navigate(ROUTES.CART)}
+                        badgeContent={cartItemsCount}
+                        badgeColor="error"
+                    />
+                    <NavButton
+                        icon={<DarkModeOutlinedIcon sx={{color:"#97A1AF"}} />}
+                        onClick={() => console.log("toggle theme")}
+                    />
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
-};
+}
